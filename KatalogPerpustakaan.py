7f1ea0a9eaa373e583931eaaf24756673b023c45
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-10-16 12:46:15 +0700

    Update KatalogPerpustakaan.py

--- KatalogPerpustakaan.py
+++ KatalogPerpustakaan.py
@@ -1,56 +1,109 @@
+import os
+
 myBook = ['Buku1','Buku2','Buku3','Buku4','Buku5']
+borrowBook = []
 
 def cariBuku(namaBuku):
     kondisi1=True
+    kondisi2=True
     for i in range (len(myBook)):
         if(namaBuku==myBook[i]):
             kondisi1=False
             break
             
     if kondisi1 == False:
-        print("Ada bukunya!")
-
+        print('Buku ' + namaBuku + ' telah ditemukan')
+        print("Tekan \"Enter\" untuk melanjutkan")
+        x = input()
+        menuUtama()
     else:
-        print("Gaada bukunya!")
+        for i in range (len(borrowBook)):
+            if(namaBuku==borrowBook[i]):
+                kondisi2=False
+                break
+        
+    if kondisi2 == False:
+        print('Buku ' + namaBuku + ' sedang dipinjam')
+        print("Tekan \"Enter\" untuk melanjutkan")
+        x = input()
+        menuUtama()
+    else:
+        print('Buku ' + namaBuku + ' tidak ditemukan')
+        print("Tekan \"Enter\" untuk melanjutkan")
+        x = input()
+        menuUtama()
+    
 
-def tambahBuku(namaBuku,jumlah):
-    for i in range (jumlah):
-        myBook.append(namaBuku)
+def tambahBuku(judulBuku):
+    myBook.append(judulBuku)
     menuUtama()
 
-def pinjamBuku(namaBuku,jumlah):
-    for i in range (jumlah):
-        myBook.remove(namaBuku)
+def hapusBuku(judulBuku):
+    myBook.remove()
+
+def pinjamBuku(judulBuku):
+    kondisi3 = True
+    for i in range (len(myBook)):
+        if(judulBuku==myBook[i]):
+            kondisi3=False
+            break
+    if kondisi3 == False:
+        myBook.remove(judulBuku)
+        borrowBook.append(judulBuku)
+    else:
+        print('Buku ', judulBuku, 'tidak ditemukan di list')
+        print("Tekan \"Enter\" untuk melanjutkan")
+        x = input()
     menuUtama()
+
+def pengembalianBuku(judulBuku):
+    kondisi4 = True
+    for i in range (len(borrowBook)):
+        if(judulBuku==borrowBook[i]):
+            kondisi3=False
+            break
+    if kondisi3 == False:
+        myBook.append(judulBuku)
+        borrowBook.remove(judulBuku)
+    else:
+        print('Buku ', judulBuku, 'tidak sedang dipinjam')
+        print("Tekan \"Enter\" untuk melanjutkan")
+        x = input()
+    menuUtama()
+
 
 def menuUtama():
-    print(myBook)
-    print("""Menu Utama:
-    1. {0}
-    2. {1}
-    3. {2}
-    9. Keluar""".format("Cari buku", "Tambah buku", "Pinjam buku"))
+    #Program akan melakukan perulangan sampai exit
+    while(True):
+        os.system('cls') #membersihkan layar
+        print('List Buku: ', myBook)
+        print('Buku yang sedang dipinjam: ', borrowBook,'\n')
+        print("""Selamat Datang di program Katalog Perpustakaan:
+        1. {0}
+        2. {1}
+        3. {2}
+        4. {3}
+        9. Keluar""".format("Cari buku", "Tambah buku", "Pinjam buku","Pengembalian Buku"))
 
-    input1 = eval(input("Masukan Pilihan: "))
+        choose = eval(input("Masukan Pilihan: "))
 
-    if input1 == 1:
-        input2 = input('Masukkan nama buku : ')
-        cariBuku(input2)
+        if choose == 1:
+            judulBuku = input('Masukkan judul buku : ')
+            cariBuku(judulBuku)
+        elif choose == 2:
+            judulBuku = input('Masukan judul buku : ')
+            tambahBuku(judulBuku)
+        elif choose == 3:
+            judulBuku = input('Masukan judul buku : ')
+            pinjamBuku(judulBuku)
+        elif choose == 4:
+            judulBuku = input('Masukan judul buku : ')
+            pengembalianBuku(judulBuku)
+        elif choose == 9:
+            print('Program telah berakhir')
+            break
+        else:
+            print('Invalid Input')
 
-    elif input1 == 2:
-        input2 = input('Masukan nama buku : ')
-        input3 = eval(input('Jumlah buku : '))
-        tambahBuku(input2,input3)
-
-    elif input1 == 3:
-        input4 = input('Masukan nama buku : ')
-        input5 = eval(input('Jumlah buku : '))
-        pinjamBuku(input4,input5)
-
-    elif input1 == 9:
-        exit
-
-    else:
-        print("GOBLOK")
-
+#Program dijalankan
 menuUtama()
