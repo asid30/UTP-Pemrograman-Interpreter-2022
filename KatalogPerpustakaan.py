34d06d1d664f7d6249d43dcfe5b872da6b89eb55
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-10-17 20:03:13 +0700

    Pilihan menu jadi menggunakan string tuple

--- KatalogPerpustakaan.py
+++ KatalogPerpustakaan.py
@@ -77,8 +77,8 @@ def denda (hari):
     x = input()
 
 def menuUtama():
+    pilihanMenu = ("Cari buku", "Tambah buku", "Pinjam buku","Pengembalian buku","Kalkulator denda")
     #Program akan melakukan perulangan sampai exit
-    
     while(True):
         os.system('cls') #membersihkan layar
         title()
@@ -90,7 +90,7 @@ def menuUtama():
         3. {2}
         4. {3}
         5. {4}
-        9. Keluar""".format("Cari buku", "Tambah buku", "Pinjam buku","Pengembalian buku","Kalkulator denda"))
+        9. Keluar""".format(pilihanMenu[0], pilihanMenu[1], pilihanMenu[2],pilihanMenu[3],pilihanMenu[4]))
 
         choose = eval(input("Masukan Pilihan: "))
 
